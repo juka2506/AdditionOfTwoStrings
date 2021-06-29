df63097dc0f7668232b4123f896688371dc1a551
Julija Katukova <julija.katukova@gmail.com>
2021-06-29 22:54:44 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String num1 = "15";
+        String num2 = "30";
+
+        int number1 = Integer.parseInt(num1);
+        int number2 = Integer.parseInt(num2);
+
+        System.out.println("\"" + num1 + "\" + \"" + num2 + "\" = " + (number1 + number2));
+    }
+}
